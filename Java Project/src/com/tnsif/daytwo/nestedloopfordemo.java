ad1b560ee529edfa39bb158506ad498226354e30
Aditi Ramdas Rakh <Aditi Ramdas Rakh@LAPTOP-3R9CK8KR>
2025-07-04 10:28:52 +0530

    Day2

---
+++ Java Project/src/com/tnsif/daytwo/nestedloopfordemo.java
@@ -0,0 +1,16 @@
+package co=begm.tnsif.daytwo .jumpdemo;
+
+public class ContinueDemo {
+	
+	public static void main (string[]args) {
+		for (int k=5; k<15; k++)
+			 
+			//odd no are skipped
+			
+			if (k%2 !=0)
+				continue;
+//even number are printed	
+	}
+		
+
+}
---
+++ Java Project/src/com/tnsif/daytwojumpdemo/Breakdemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.daytwojumpdemo;
+
+public class Breakdemo {
+	  
+	public static void main (String[] args) {
+		
+		for (int i=5; i<10; i++;)
+		{
+			    //break;
+			System.out.prin);
+		}
+	}
+
+}
