e0d4a1ad81f468e3a63a67cf9b2c1d852a45dfcf
Aditi Ramdas Rakh <Aditi Ramdas Rakh@LAPTOP-3R9CK8KR>
2025-07-07 00:43:06 +0530

    Day Two

---
+++ day2/src/day2/IFDemo.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class IFDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=3;
+		
+		if(x == 4 )
+			System.out.println("Value of x is 4");
+
+		
+	}
+
+
+}
